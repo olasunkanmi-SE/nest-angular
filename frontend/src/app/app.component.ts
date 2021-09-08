7128d76dfabbbecdc5b67f0b9bd446e0da9b4506
Olasunkanmi Raymond <raymond.oyinlola@gmail.com>
2021-09-08 11:38:52 +0800

    resolve cors error

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  baseURL: string = environment.backendAPI;
   title = 'frontend';
+  backendMsg$: string = '';
+  constructor(private http: HttpClient) {
+    this.showMessage();
+  }
+
+  showMessage() {
+    this.getBackendMsg()
+      .pipe()
+      .subscribe((res) => {
+        this.backendMsg$ = res;
+      });
+  }
+
+  getBackendMsg(): Observable<string> {
+    return this.http.get(`${this.baseURL}`, { responseType: 'text' });
+  }
 }
